83bbe0951e7d84258cfffef34649f302221c9bac
Zering <550727632@qq.com>
2017-09-18 23:38:40 +0800

    java api实现更新和删除elasticSearch数据信息

--- src/main/java/com/elasticsearch/Application.java
+++ src/main/java/com/elasticsearch/Application.java
@@ -2,8 +2,11 @@ package com.elasticsearch;
 
 import java.io.IOException;
 import java.util.Date;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -13,10 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -81,6 +85,50 @@ public class Application {
     }
     return new ResponseEntity(result.getSource(), HttpStatus.OK);
   }
+
+  @PutMapping("/update/book/novel")
+  public ResponseEntity update(
+      @RequestParam(name = "id") String id,
+      @RequestParam(name = "title", required = false) String title,
+      @RequestParam(name = "author", required = false) String author){
+    UpdateRequest request = new UpdateRequest("book", "novel", id);
+    try {
+      XContentBuilder builder = XContentFactory.jsonBuilder();
+
+      builder.startObject();
+      if (title != null) {
+        builder.field("title", title);
+      }
+      if (author != null){
+        builder.field("author", author);
+      }
+      builder.endObject();
+
+      request.doc(builder);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    try {
+      UpdateResponse result = this.client.update(request).get();
+      return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+  }
+
+  @DeleteMapping("/delete/book/novel")
+  public ResponseEntity delete(
+      @RequestParam(name = "id") String id
+  ) {
+    DeleteResponse result = this.client.prepareDelete("book", "novel", id).get();
+    return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
