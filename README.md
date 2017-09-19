179fedaeb90bd0b36ced608c3f19bdaee3ba5d97
Zering <550727632@qq.com>
2017-09-19 23:15:27 +0800

    java api实现复合查询elasticSearch数据信息

---
+++ README.md
@@ -0,0 +1,22 @@
+### 学习慕课网瓦力老师的ElasticSearch入门课程
+
+只有自己动手写一边 才能知道其中有哪些坑
+
+过程中遇到的问题：
+
+1. 在分布式部署多节点时，slave复制了data文件夹，导致slave无法启动
+2. requertparam时间格式是带时区的，注意转换
+3. log4j版本问题，导致TransportClient初始化失败
+4. 使用postman时，注意Content-Type的区别
+5. 有了@RestController 就不需要在方法上加 @ResponseBody了
+
+
+
+视频地址:
+
+[ElasticSearch入门](http://www.imooc.com/learn/889)
+
+
+
+题外话：推荐一款写markdown很爽的免费软件  [Typora](https://www.typora.io/)
+
--- src/main/java/com/elasticsearch/Application.java
+++ src/main/java/com/elasticsearch/Application.java
@@ -1,15 +1,25 @@
 package com.elasticsearch;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,14 +34,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * elasticsearch java api
+ *
  * Created by zhanghaojie on 2017/9/17.
  */
 @SpringBootApplication
 @RestController
 public class Application {
 
+  private final TransportClient client;
+
   @Autowired
-  private TransportClient client;
+  public Application(TransportClient client) {
+    this.client = client;
+  }
 
   @GetMapping("/")
   public String index() {
@@ -39,7 +55,7 @@ public class Application {
   }
 
   @PostMapping("/add/book/novel")
-  public ResponseEntity add(
+  public ResponseEntity<String> add(
       @RequestParam(name = "title") String title,
       @RequestParam(name = "author") String author,
       @RequestParam(name = "word_count") int wordCount,
@@ -62,32 +78,32 @@ public class Application {
           .setSource(builder)
           .get();
 
-      return new ResponseEntity(result.getId(), HttpStatus.OK);
+      return new ResponseEntity<>(result.getId(), HttpStatus.OK);
 
     } catch (IOException e) {
       e.printStackTrace();
-      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
   }
 
   @GetMapping("/get/book/novel")
-  public ResponseEntity get(@RequestParam(name = "id", defaultValue = "") String id) {
+  public ResponseEntity<Map<String, Object>> get(@RequestParam(name = "id", defaultValue = "") String id) {
     if (id.isEmpty()) {
-      return new ResponseEntity(HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     GetResponse result = this.client.prepareGet("book", "novel", id).get();
 
     if (!result.isExists()) {
-      return new ResponseEntity(HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    return new ResponseEntity(result.getSource(), HttpStatus.OK);
+    return new ResponseEntity<>(result.getSource(), HttpStatus.OK);
   }
 
   @PutMapping("/update/book/novel")
-  public ResponseEntity update(
+  public ResponseEntity<String> update(
       @RequestParam(name = "id") String id,
       @RequestParam(name = "title", required = false) String title,
       @RequestParam(name = "author", required = false) String author){
@@ -108,25 +124,69 @@ public class Application {
 
     } catch (IOException e) {
       e.printStackTrace();
-      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     try {
       UpdateResponse result = this.client.update(request).get();
-      return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+      return new ResponseEntity<>(result.getResult().toString(), HttpStatus.OK);
     } catch (Exception e) {
       e.printStackTrace();
-      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
   }
 
   @DeleteMapping("/delete/book/novel")
-  public ResponseEntity delete(
+  public ResponseEntity<String> delete(
       @RequestParam(name = "id") String id
   ) {
     DeleteResponse result = this.client.prepareDelete("book", "novel", id).get();
-    return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+    return new ResponseEntity<>(result.getResult().toString(), HttpStatus.OK);
+  }
+
+  @PostMapping("/query/book/novel")
+  public ResponseEntity<List<Map<String, Object>>> query(
+      @RequestParam(name = "title", required = false) String title,
+      @RequestParam(name = "author", required = false) String author,
+      @RequestParam(name = "gt_word_count", defaultValue = "0") int gtWordCount,
+      @RequestParam(name = "lt_word_count", required = false) int ltWordCount
+  ){
+    BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+    if (title != null) {
+      boolQueryBuilder.must(QueryBuilders.matchQuery("title", title));
+    }
+
+    if (author != null) {
+      boolQueryBuilder.must(QueryBuilders.matchQuery("author", author));
+    }
+
+    RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("word_count");
+
+    rangeQuery.from(gtWordCount);
+    if (ltWordCount > 0) {
+      rangeQuery.to(ltWordCount);
+    }
+    boolQueryBuilder.filter(rangeQuery);
+
+    SearchRequestBuilder builder = this.client.prepareSearch("book")
+        .setTypes("novel")
+        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+        .setQuery(boolQueryBuilder)
+        .setFrom(0)
+        .setSize(10);
+
+    System.out.println(builder);
+
+    SearchResponse response = builder.get();
+    List<Map<String, Object>> result = new ArrayList<>();
+    for (SearchHit hitFields : response.getHits()){
+      result.add(hitFields.getSource());
+    }
+
+    return new ResponseEntity<>(result, HttpStatus.OK);
+
   }
 
 
